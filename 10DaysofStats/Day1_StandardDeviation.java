9a12194f22a91b6ae04b5f01eec7a29c08accc10
Marovski <cardozo27cv@gmail.com>
2018-08-26 23:56:31 +0100

    Update Day1_StandardDeviation.java

--- 10DaysofStats/Day1_StandardDeviation.java
+++ 10DaysofStats/Day1_StandardDeviation.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class Solution {
 
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        /*Print output to STDOUT. Your class should be named Solution. */
         
         Scanner in = new Scanner(System.in);
         
