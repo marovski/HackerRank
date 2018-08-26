355bfcb100602894ea6bacff9738a1430b71b620
Marovski <cardozo27cv@gmail.com>
2018-08-26 23:56:04 +0100

    Update Day0_MeanModeMedian.java
    Adjust some spacing

--- 10DaysofStats/Day0_MeanModeMedian.java
+++ 10DaysofStats/Day0_MeanModeMedian.java
@@ -7,20 +7,16 @@ import java.text.DecimalFormat;
 public class Solution {
 
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        /* Read input from STDIN. Print output to STDOUT.  */
         DecimalFormat df = new DecimalFormat("#.#");
         Scanner in = new Scanner(System.in);
         int t = in.nextInt();
         int[] anArray = new int[t];
 
-       
-              
-                
         for(int i=0;i<t;i++){
             anArray[i]=in.nextInt();                  
               }
-       
-
+        
         System.out.println( df.format( mean(anArray, t)));
         System.out.println( df.format(median(anArray)));
         System.out.println(mode(anArray));
@@ -40,13 +36,13 @@ public class Solution {
     }
       public static double median(int[] numArray){
        Arrays.sort(numArray);
-        double median;
-        if (numArray.length % 2 == 0)
-       median = ((double)numArray[numArray.length/2] + (double)numArray[numArray.length/2 - 1])/2;
-        else
-         median = (double) numArray[numArray.length/2];
+       double median;
           
-          return median;
+       if (numArray.length % 2 == 0)
+       median = ((double)numArray[numArray.length/2] + (double)numArray[numArray.length/2 - 1])/2;
+        else median = (double) numArray[numArray.length/2];
+          
+         return median;
     }
     
     
